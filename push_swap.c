43111ffe442b40c17cdcdbc54b75904d174ddda9
Dinursult <give.me.kitchen@gmail.com>
2025-02-23 15:49:18 +0100

    small changes

--- push_swap.c
+++ push_swap.c
@@ -6,20 +6,14 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 11:04:42 by Dias              #+#    #+#             */
-/*   Updated: 2025/02/23 14:28:24 by Dias             ###   ########.fr       */
+/*   Updated: 2025/02/23 15:43:38 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char *av[])
+int	ft_sanitize(t_stack *a)
 {
-	t_stack	*a;
-	t_stack	*b;
-
-	if (ac < 2)
-		return (0);
-	a = ft_parse(av);
 	if (!a || !a->top || ft_check_duplicates(a))
 	{
 		write (2, "Error\n", 6);
@@ -31,6 +25,19 @@ int	main(int ac, char *av[])
 		ft_clean_stack(a);
 		return (0);
 	}
+	return (1);
+}
+
+int	main(int ac, char *av[])
+{
+	t_stack	*a;
+	t_stack	*b;
+
+	if (ac < 2)
+		return (0);
+	a = ft_parse(av);
+	if (!ft_sanitize(a))
+		return (0);
 	b = ft_init_stack();
 	if (!b)
 	{
